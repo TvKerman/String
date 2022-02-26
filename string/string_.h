ce5d5f2ad4072df2fe1fe9debb26a8e7ad04a37f
Kerman <grigorij.perfilin@mail.ru>
2022-02-26 14:47:35 +0300

    append strlen_ function

--- string/string_.h
+++ string/string_.h
@@ -4,4 +4,15 @@
 #ifndef STRING_STRING__H
 #define STRING_STRING__H
 
+#include <stdlib.h>
+
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0') {
+        end++;
+    }
+
+    return end - begin;
+}
+
 #endif //STRING_STRING__H
